068f8581bd0c6ec26230a9576fb73280d59f4c3a
Beno Dwianto <benodwianto9@gmail.com>
2024-11-03 15:16:44 +0700

    day 8 ex 2

---
+++ volumeCuboit.php
@@ -0,0 +1,8 @@
+<?php
+// Day 8 exercise 2
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/php
+
+function get_volume_of_cuboid($length, $width, $height)
+{
+    return ($length * $width * $height);
+}
